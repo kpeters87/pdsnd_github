d7eefc18010fe7d9c7edace3f79ad53971de09c8
Kurt Peters <kurtpeters87@gmail.com>
2020-09-07 11:06:34 -0400

    Fixing mistake on reset

---
+++ filter.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import city as ct
+
+def filter(df, month, day):
+    list_month = ['All month', 'Jan', 'Fev', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    list_days = ['All days', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    s_month = list_month.index(month)
+    s_day = list_days.index(day)
+    # filter by month where 0 is all month
+
+    if s_month != 0:
+        df = df[(pd.DatetimeIndex(df['Start Time']).month == s_month)]
+        if df.empty == True:
+            return "No data to the this combination of day and month, try again"
+    #filter by day where 0 is all days
+
+    if s_day != 0:
+        df = df[(pd.DatetimeIndex(df['Start Time']).weekday == s_day)]
+        if df.empty == True:
+            return "No data to the this combination of day and month, try again"
+    return df
+
+if __name__ == "__main__":
+    desired_width = 320
+    pd.set_option('display.width', desired_width)
+    pd.set_option('display.max_columns', 20)
+    head_size = 3
+    df = ct.city("Chicago")
+    df = filter(df, 'All month', 'All days' )
+    if not isinstance(df, pd.DataFrame):
+        print('é')
+    else:
+        print('nao é ')
+    print(df)
+
+    # df = ct.city(2)
+    # df = filter(df, 3, 2 )
+    # print(df.head(head_size))
+    #
+    # df = ct.city(3)
+    # df = filter(df, 3, 2 )
+    # print(df.head(head_size))
---
+++ main_project_bikeshares.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import time
+
+import city as ct
+import filter as ft
+import user_selections as usel
+import popular_time as pt
+import station_stats as st
+import trip_duration as td
+import user_stats as usta
+
+
+# list the cites to make easy to avoid typing error
+cites = ['Chicago', 'New York City', 'Washington']
+# Call a list to show the selection and keep to avoid problems
+test = ''
+while test != 'q':
+    while True:  # This loop make sure that the dataframe is not empty
+        #  call function to help the selection of cites month e days
+        user_city = usel.user_sections('cities')
+        user_month = usel.user_sections('month')
+        user_day = usel.user_sections('days')
+        df = ct.city(user_city)
+        df = ft.filter(df, user_month, user_day)
+        if isinstance(df, pd.DataFrame):
+            break
+        else:
+            print(df)
+    start_time = time.time()
+    pt.popular_time(df)
+    st.station_stats(df)
+    td.trip_duration_stats(df,user_month, user_day)
+    usta.user_stats(df)
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-' * 40)
+    test = input("Press q to exit, or something else to explore again:")
+    print("Thank you!")
