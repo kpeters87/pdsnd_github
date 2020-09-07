38b2a0dbcc51f3f264ef7736abc9b50b8d28e464
Kurt Peters <kurtpeters87@gmail.com>
2020-09-07 14:11:03 -0400

    Add Function Filter to file with Functions

--- functions.py
+++ functions.py
@@ -18,3 +18,21 @@ def city(city_index):
     df['hour'] = pd.DatetimeIndex(df['Start Time']).hour
     return df
 
+def filter(df, month, day):
+    list_month = ['All month', 'Jan', 'Fev', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    list_days = ['All days', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    s_month = list_month.index(month)
+    s_day = list_days.index(day)
+    # filter by month where 0 is all month
+
+    if s_month != 0:
+        df = df[(pd.DatetimeIndex(df['Start Time']).month == s_month)]
+        if df.empty == True:
+            return "No data to the this combination of day and month, try again"
+    #filter by day where 0 is all days
+
+    if s_day != 0:
+        df = df[(pd.DatetimeIndex(df['Start Time']).weekday == s_day)]
+        if df.empty == True:
+            return "No data to the this combination of day and month, try again"
+    return df
