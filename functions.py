7f86561b839d82fd5f3834308fec01f1ada07935
Kurt Peters <kurtpeters87@gmail.com>
2020-09-07 14:30:17 -0400

    Added Poupular_tiem to functions.py

--- functions.py
+++ functions.py
@@ -5,6 +5,8 @@ CITY_DATA = { 'Chicago': 'chicago.csv',
               'New York City': 'new_york_city.csv',
               'Washington': 'washington.csv' }
 
+list_month = ['All month', 'Jan', 'Fev', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
 def user_sections(type_of_selection):
     list_cites = ['Chicago', 'New York City', 'Washington']
     list_month = ['All month', 'Jan', 'Fev', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -58,7 +60,7 @@ def filter(df, month, day):
     Returns:
     (df) Pandas DataFrame - DataFrame filtered with choosen data from user
     """
-    list_month = ['All month', 'Jan', 'Fev', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    
     list_days = ['All days', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     s_month = list_month.index(month)
     s_day = list_days.index(day)
@@ -75,3 +77,27 @@ def filter(df, month, day):
         if df.empty == True:
             return "No data to the this combination of day and month, try again"
     return df
+
+def popular_time(df):
+    
+    list_month_dict = dict(enumerate(list_month))
+    list_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday']
+    list_days_dict = dict(enumerate(list_days))
+
+    # display the most common month
+    group_month = df.groupby('month').size().to_frame(name='count').reset_index()
+    group_month['month'] = group_month['month'].map(list_month_dict)
+    group_month.set_index('month',inplace=True)
+    print(group_month.sort_values('count',ascending=False).head(5).to_string())
+
+    # display the most common day of week
+    group_week_day = df.groupby('week_day').size().to_frame(name='count').reset_index()
+    group_week_day['week_day'] = group_week_day['week_day'].map(list_days_dict)
+    group_week_day.set_index('week_day',inplace=True)
+    print(group_week_day.sort_values('count',ascending=False).head(5).to_string())
+
+    # display the most common start hour
+    group_hour = df.groupby('hour').size().to_frame(name='count').reset_index()
+    group_hour.set_index('hour')
+    group_hour.set_index('hour',inplace=True)
+    print(group_hour.sort_values('count',ascending=False).head(5).to_string())
