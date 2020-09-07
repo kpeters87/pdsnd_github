16b7879e2f8c7fa9546866fb5ef7e5afaece2474
Kurt Peters <kurtpeters87@gmail.com>
2020-09-07 14:51:28 -0400

    added user_stats to functions

--- functions.py
+++ functions.py
@@ -128,3 +128,23 @@ def trip_duration_stats(df, user_month, user_day):
     print("The mean travel time for {}, selecting {} is: {}".format(user_month, user_day, str(df['Time Difference'].mean()).split(".")[0]))
 
     print('-'*40)
+
+def user_stats(df):
+    if __name__ == "__main__":
+        print(df)
+        print(df['User Type'].isnull().sum())
+    # Display counts of user types
+    group_user_type = df.groupby('User Type').size().to_frame(name='count')
+    print(group_user_type)
+    print('-' * 20)
+
+    # Display counts of gender
+    if 'Gender' in df:
+        group_user_gender = df.groupby('Gender').size().to_frame(name='count')
+        print(group_user_gender)
+        print('-' * 20)
+    # Display earliest, most recent, and most common year of birth
+        print("The oldest person was born in: {}".format(int(df['Birth Year'].max())))
+        print("The youngest person was born in: {}".format(int(df['Birth Year'].min())))
+        print("The most common year of birth is: {}".format(int(df['Birth Year'].mode())))
+    print('-' * 40)
