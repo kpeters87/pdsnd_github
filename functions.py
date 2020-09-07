90ff1e5f5a8c9c3c95232bb2a8f0e878a13dc661
Kurt Peters <kurtpeters87@gmail.com>
2020-09-07 14:02:33 -0400

    Catching someting wierd

---
+++ functions.py
@@ -0,0 +1,10 @@
+    """
+    This file has a function to get together all created functions for the project.
+
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
